860977cb9cd6aab228e92db1f5a489a4288f9bf4
Williamjacobsen <villi05.v.j@gmail.com>
2025-09-08 13:30:06 +0200

    parser setup

---
+++ backend/internal/parser/node.go
@@ -0,0 +1,9 @@
+package parser
+
+// TODO: Make type HTMLNodeElement
+// it stores the position of the element in the html buffer
+
+type HTMLNodeElement struct {
+	Tag      string
+	Position int
+}
---
+++ backend/internal/parser/parser.go
@@ -0,0 +1,14 @@
+package parser
+
+/*Returns an element of type HTMLNodeElement*/
+func GetElementByXpath() HTMLNodeElement {
+	return HTMLNodeElement{}
+}
+
+func CountChildren() {
+
+}
+
+func GetHref() {
+
+}
