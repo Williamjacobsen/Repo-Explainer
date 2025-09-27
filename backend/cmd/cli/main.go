68e7280cd0ac7e447081c674252b6a8593b7b135
Williamjacobsen <villi05.v.j@gmail.com>
2025-09-27 22:37:45 +0200

    starting over because the data im looking for is not in the html :(

--- backend/cmd/cli/main.go
+++ backend/cmd/cli/main.go
@@ -1,9 +1,7 @@
 package main
 
 import (
-	fetch_tree "github.com/Williamjacobsen/Repo-Explainer/backend/internal/service"
 )
 
 func main() {
-	fetch_tree.FetchTree("https://github.com/Williamjacobsen/ClosedAI/tree/main")
 }
