d044ded106e279fbc16ec39618f7de75ab9eba8e
Williamjacobsen <villi05.v.j@gmail.com>
2025-09-28 16:35:50 +0200

    print github repo structure in cli

--- backend/internal/formatter/ascii.go
+++ backend/internal/formatter/ascii.go
@@ -5,10 +5,67 @@ import (
 	"strings"
 )
 
+type node struct {
+	name     string
+	children []*node
+	isFile   bool
+}
+
 func UrlsToAsciiTree(urls []string) {
-	for _, s := range urls {
-		path := strings.Split(s, "/")
-		path = path[8:]
-		fmt.Println(strings.Join(path, "/"))
+	fmt.Println("\nGithub Repository Structure:")
+	for i, s := range urls {
+		nodes := strings.Split(s, "/")
+		nodes = nodes[8:]
+		urls[i] = strings.Join(nodes, "/")
+	}
+
+	tree := constructTree(urls)
+	printTree(tree)
+}
+
+func constructTree(urls []string) *node {
+	root := newNode("ROOT")
+
+	for _, url := range urls {
+		urlNodes := strings.Split(url, "/")
+		curr := root
+
+		for _, urlNode := range urlNodes {
+			curr = findOrCreateChild(curr, urlNode)
+		}
+		curr.isFile = true
+	}
+
+	return root
+}
+
+func findOrCreateChild(parent *node, name string) *node {
+	for _, child := range parent.children {
+		if child.name == name {
+			return child
+		}
+	}
+	child := newNode(name)
+	parent.children = append(parent.children, child)
+	return child
+}
+
+func newNode(name string) *node {
+	return &node{name: name}
+}
+
+func printTree(tree *node) {
+	printTreeHelper(tree, 0)
+}
+
+func printTreeHelper(tree *node, depth int) {
+	if tree.isFile {
+		fmt.Println(strings.Repeat(" ", depth*2) + tree.name)
+	} else {
+		fmt.Println(strings.Repeat(" ", depth*2) + tree.name + "/")
+	}
+
+	for _, child := range tree.children {
+		printTreeHelper(child, depth+1)
 	}
 }
